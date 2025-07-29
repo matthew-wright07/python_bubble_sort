b032bde2518ce69968dc8e7ee1c0a2e590855430
Matthew <matthewjwright07@gmail.com>
2025-07-29 14:20:26 -0500

    updates

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import random
-import matplotlib.pyplot as plt
 
 numbers = []
-
-plt.ion()
-fig, ax = plt.subplots()
 
 for _ in range(100):
     numbers.append(random.randint(1,100))
@@ -16,15 +12,8 @@ for _ in range(len(numbers)-1):
             inbetween = numbers[i]
             numbers[i] = numbers[i+1]
             numbers[i+1] = inbetween
-            if i % 10 == 0:
-                if not plt.fignum_exists(fig.number):
-                    plt.ioff()
-                    exit()  
-                plt.clf()
-                plt.bar(range(len(numbers)),numbers)
-                plt.pause(0.01)
             swapped = True
     if not swapped:
         break
 
-plt.ioff() 
+print(numbers)
